6b505a4d8dd5b4ec92bc75323dd559222c16dbf3
kleisy <kleacor25@gmail.com>
2022-11-24 10:05:14 -0500

    clase 03 completada

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ const btn = document.querySelector("[data-form-btn]");
 // funciones anonimas
 const createTask = (evento)=>{
       evento.preventDefault();
-     const input = document.querySelector('[data-form-input]');   console.log(input.value); 
+     const input = document.querySelector('[data-form-input]');
+      const value =  input.value;
+      input.value ="";
+
+      //creando elemento li para agregar las tareas
+     const task = document.createElement("li");
+    
+     //agregando la clase de forma dinamica para conservar el estilo
+     task.classList.add('card');
+
+     //objeto padre
+     const list = document.querySelector("[data-list]");
+
+     const content = ` <div>
+     <i class="far fa-check-square icon"></i>
+     <span class="task">${value}</span>
+   </div>
+   <i class="fas fa-trash-alt trashIcon icon"></i>`; //usando templeate strings para actualizar la cardlist pos0
+
+   //agregando elemento
+   task.innerHTML = content; //asignamos el cod. html
+
+   //agregando elemento hijo
+   list.appendChild(task);
+     console.log(content); 
 }
 console.log(btn);
 
